be3dea3298bc2ca1595941bd98a04c5b326dab6a
arshiya.mahmoudi <arshiya.mahmoudi@aut.ac.ir>
2023-03-08 05:01:53 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-sudo apt install docker
+sudo apt install -qq -y --no-install-recommends docker xdotool
+while true; do xdotool key ctrl; sleep 60; done &
+cd nasirdocker
 docker build --file ./Dockerfile .
