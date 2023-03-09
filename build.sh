007c286ab0524a6dc3b60d7220b805189dc56605
Your Name <you@example.com>
2023-03-09 12:15:26 +0330

    removed -qq as it became all quiet

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo apt install -qq -y --no-install-recommends docker xdotool
+sudo apt install -y --no-install-recommends docker xdotool
 while true; do xdotool key ctrl; sleep 60; done &
 ROOT=~/Programming
 mkdir $ROOT
